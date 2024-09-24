f0b9f596f2c6c259b3a8f342c6ec1263a15ef22e
PythonTamer <cakazizilham@gmail.com>
2024-09-25 05:52:50 +0700

    exit while

--- todos_app.py
+++ todos_app.py
@@ -1,19 +1,21 @@
 print("APLIKASI TO-DO LIST")
 
 
-nama = input("Masukkan Nama Anda : ")
-jumlah = int(input("Masukkan Jumlah Tugas : "))
 tugas = []
-
-for i in range(0, jumlah):
+nama = input("Masukkan Nama Anda : ")
+while True:
     todo = input(f"Masukkan Tugas: ")
-    tugas.append(todo)
-    
-print(f"Selamat datang {nama}, ada memiliki beberapa kegiatan yaitu : {tugas}, tugas anda sebanyak : {len(tugas)}")
-exit_ = input("Apakah anda ingin keluar ? (y/n) : ")
-if exit_ == "y":
-    print("Terimakasih")
-else:
-    print("Program akan diulang")
-    exit()
-    
+    tugas.append(todo) 
+    if todo == "stop":
+#jumlah = int(input("Masukkan Jumlah Tugas : "))
+#for i in range(0, jumlah):
+ #   todo = input(f"Masukkan Tugas: ")
+  #  tugas.append(todo) ***  
+        print(f"Selamat datang {nama}, ada memiliki beberapa kegiatan yaitu : {tugas}, tugas anda sebanyak : {len(tugas)}")
+        exit_ = input("Apakah anda ingin keluar ? (y/n) : ")
+        if exit_ == "y":
+            print("Terimakasih")
+            break
+        else:
+          print("Tambahkan tugas dan ketik stop jika ingin berhenti")
+        
