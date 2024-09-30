6cc23cacc55b1d7c15e0043538997c5d8a086b8f
PythonTamer <cakazizilham@gmail.com>
2024-09-30 22:43:58 +0700

    filter function

--- todos_app.py
+++ todos_app.py
@@ -1,36 +1,52 @@
 print("APLIKASI TO-DO LIST")
-nama = input("Masukkan Nama : ")
-tugas = []
+
+def name():
+    nama = input("Masukkan Nama Depan : ")
+    nama1 = input("Masukkan Nama Belakang : ")
+    return f"{nama} {nama1}"
+
+def tampilnama(fullname):
+    print(f"Selamat Datang {fullname}")
+
+    
 def menu():
     print("1. Tambahkan Tugas")
     print("2. Lihat Tugas")
     print("3. Hapus Tugas")
     print("4. Keluar")
+
+def user_input_cek():
     while True:
      try:
           user_input = int(input("Masukkan Pilihan : "))
-          break
+          if user_input in [1,2,3,4]:
+             return user_input
+          else:
+             print("Pilihan Tidak Tersedia")
      except ValueError:
            print("Inputan harus berupa angka")
-def user():
-    if user_input > 4:
-        while user_input > 4:
-            user_input = int(input("Masukkan Pilihan : "))
-    elif user_input == 1:
+def user_menu(user_input):
+    if user_input == 1:
         print("1. Tambahkan Tugas")
+        data()
     elif user_input == 2:
         print("2. Lihat Tugas")
+        tampil()
     elif user_input == 3:
         print("3. Hapus Tugas")
     elif user_input == 4:
         print("4. Keluar")
+        keluar()
 def data():
-    todo = input(f"Masukkan Tugas: ")
+    todo = ""
+    global tugas
+    tugas = []
     while todo.lower() != "stop":
-        tugas.append(todo) 
+        tugas.append(todo)
         todo = input(f"Masukkan Tugas: ")
+    return menu(), user_input_cek(), tampil()
 def tampil():
-    print(f"Selamat datang {nama}, ada memiliki beberapa kegiatan yaitu : {tugas}, tugas anda sebanyak : {len(tugas)}")
+    print(f"Selamat datang {fullname}, ada memiliki beberapa kegiatan yaitu : {tugas}, tugas anda sebanyak : {len(tugas)}")
 def keluar():    
     exit_ = input("Apakah anda ingin keluar (y/n) : ")
     if exit_ == 'y':
@@ -40,8 +56,9 @@ def keluar():
         print("Silahkan tambahkan tugas anda: ")
         data()
         tampil()
+
+fullname = name()
+tampilnama(fullname)
 menu()
-data()
-tampil() 
-keluar()
-   
+user_input = user_input_cek()
+user_menu(user_input)
