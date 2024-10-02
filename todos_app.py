9408f30bfac6d9861c3f59d018a5ae0efc366d51
PythonTamer <cakazizilham@gmail.com>
2024-10-02 21:43:06 +0700

    hampir selesai

--- todos_app.py
+++ todos_app.py
@@ -1,5 +1,6 @@
 print("APLIKASI TO-DO LIST")
 
+user_input = int
 def name():
     nama = input("Masukkan Nama Depan : ")
     nama1 = input("Masukkan Nama Belakang : ")
@@ -15,6 +16,7 @@ def menu():
     print("3. Hapus Tugas")
     print("4. Keluar")
 
+
 def user_input_cek():
     while True:
      try:
@@ -25,7 +27,7 @@ def user_input_cek():
              print("Pilihan Tidak Tersedia")
      except ValueError:
            print("Inputan harus berupa angka")
-def user_menu(user_input):
+def user_menu():
     if user_input == 1:
         print("1. Tambahkan Tugas")
         data()
@@ -38,11 +40,15 @@ def user_menu(user_input):
         except NameError:
             print("Masukkan tugas anda terlebih dahulu")           
         tampil()
+        user_input_cek()
     elif user_input == 3:
-        print("3. Hapus Tugas")
+        hapus()
+        user_input_cek()
     elif user_input == 4:
-        print("4. Keluar")
         keluar()
+        print("4. Apakah Anda yakin akan keluar ?")
+
+
 def data():
     todo = input(f"Masukkan Tugas: ")
     global tugas
@@ -50,15 +56,22 @@ def data():
     while todo.lower() != "stop":
         tugas.append(todo)
         todo = input(f"Masukkan Tugas: ")
-    return menu(), user_input_cek(), tampil()
+    return user_input_cek()
+
+def hapus():
+    tugas_hapus = int(input("Hapus Tugas ke : ")) - 1
+    tugas.pop(tugas_hapus)
+    return user_input_cek()
 
 def tampil():
         print(f"Selamat Datang {fullname}, tugas Anda adalah:")
-        num = 1 
+        global tugas
+        num = 1
         for i in tugas:
             print(f"{num}.{i}")
             num += 1
         print(f"Jumlah, tugas anda sebanyak : {len(tugas)}")
+        return user_input_cek()
 def keluar():    
     exit_ = input("Apakah anda ingin keluar (y/n) : ")
     if exit_ == 'y':
@@ -72,5 +85,11 @@ def keluar():
 fullname = name()
 tampilnama(fullname)
 menu()
-user_input = user_input_cek()
-user_menu(user_input)
+user_input_cek()
+user_menu()
+data()
+tampil()
+hapus()
+keluar() 
+#user_input = user_input_cek()
+#user_menu(user_input)
